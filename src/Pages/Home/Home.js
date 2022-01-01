2dc53b6ce9d997405a99b2bca695f46b98bbed84
aushamim <au.shamim0@gmail.com>
2022-01-02 00:45:24 +0600

    Nav Done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import Navigation from "../../Components/Navigation/Navigation";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <Navigation></Navigation>
     </div>
   );
 };
