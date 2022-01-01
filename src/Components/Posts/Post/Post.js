1da80cd6d987a54ae331a7a96d963d9cdca255c3
Alok Kumar <alok61.bd@gmail.com>
2022-01-02 02:09:56 +0600

    post setting button add

--- src/Components/Posts/Post/Post.js
+++ src/Components/Posts/Post/Post.js
@@ -1,34 +1,41 @@
-import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
 import React from 'react';
-
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 const post = ({post}) => {
     const{userName,profile_img,Post,dateTime,image}=post;
+    const handlePost=()=>{
+        console.log('hello i am workking')
+    }
     return (
-        <div>
-             <Card sx={{ maxWidth: 345, marginLeft:'auto',marginRight:'auto' }}>
+        <div style={{marginTop:'50px'}}>
+             <Card sx={{ width:600, marginLeft:'auto',marginRight:'auto',padding:3 }}>
       <CardHeader
         avatar={
-          <Avatar sx={{background:'red'}} aria-label="recipe">
-            R
-          </Avatar>
+            <Avatar alt={userName} src={profile_img} />
         }
-       
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        action={
+            <IconButton onClick={handlePost} aria-label="settings">
+              <MoreVertIcon/>
+            </IconButton>
+          }
+        title={userName}
+        subheader={dateTime}
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
+     
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+        {Post}
         </Typography>
       </CardContent>
+    {
+        image&&<CardMedia
+        style={{borderRadius:4}}
+        component="img"
+        height="194"
+        image={image}
+        alt="Paella dish"
+      />
+    }
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
         </IconButton>
