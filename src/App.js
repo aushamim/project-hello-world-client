67ce067dece640719497e52c753f61505308a7d2
aushamim <au.shamim0@gmail.com>
2022-01-01 21:01:53 +0600

    Home Bugfix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import { Box } from "@mui/system";
 
 function App() {
   return (
