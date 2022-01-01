b9c4df1a2a52070efbe8bcba4608ea7c23175faa
Alok Kumar <alok61.bd@gmail.com>
2022-01-02 00:15:11 +0600

    Post add in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Post from "../../Components/Post/Post";
 
 const Home = () => {
   return (
     <div>
       <h1>Hello World</h1>
       <h2>This is Homepage</h2>
+      <Post></Post>
     </div>
   );
 };
