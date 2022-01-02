822596bce9a6320b37257335f3d79f25a0f8957d
Alok Kumar <alok61.bd@gmail.com>
2022-01-02 12:45:03 +0600

    fetch data

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Contact from './Contact/Contact';
 
 const Contacts = () => {
+    const [contacts,setContacts]=useState([]);
+    const [isLoading,setIsLoading]=useState(false);
+    useState(()=>{
+        fetch('./fakeData.json')
+        .then(res=>res.json())
+        .then(data=>setContacts(data))
+    },[])
     return (
         <div>
             <h1>hello i am contacts</h1>
-            <Contact></Contact>
+            {
+                contacts.map(contact=><Contact 
+                    key={contact.id}
+                    contact={contact}
+                    ></Contact>)
+            }
         </div>
     );
 };
