2a76bc236e495c346809590c912854ead2527a48
moshiur01 <mr.moshiur314@gmail.com>
2022-01-02 19:22:43 +0600

    auth provider added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Header from "./Components/Header/Header";
 import Login from "./Pages/Login/Login";
+import AuthProvider from "./Components/ContextApi/AuthProvider/AuthProvider";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header></Header>
-        <Routes>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/" element={<Home></Home>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route path="/login" element={<Login></Login>}></Route>
+            <Route path="/" element={<Home></Home>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
