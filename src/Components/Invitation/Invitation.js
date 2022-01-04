f83ad5ee7da84621a4a0bb10795db752cc7d8fc8
Alok Kumar <alok61.bd@gmail.com>
2022-01-04 18:14:05 +0600

    working on invitatin

--- src/Components/Invitation/Invitation.js
+++ src/Components/Invitation/Invitation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Typography, typographyClasses} from '@mui/material';
+import {Box, Typography, } from '@mui/material';
 const Invitation = () => {
     return (
        <Box sx={{
@@ -8,9 +8,19 @@ const Invitation = () => {
            borderRadius:5,
            marginTop:5,
            }}>
-            <Typography>
-                hello world , i am invitation
-            </Typography>
+           <Box
+           sx={{
+              height:'200px',
+              border:'2px solid green',
+              backgroundImage:"url('https://i.ibb.co/XkDvzjT/Darjeeling-Gangtok-Tour-Package-Small.jpg')",
+              backgroundPosition:'cover',
+              backgroundRepeat:'no-repeat'
+           }}
+           >
+               <Typography>
+                   its world
+               </Typography>
+           </Box>
        </Box>
     );
 };
